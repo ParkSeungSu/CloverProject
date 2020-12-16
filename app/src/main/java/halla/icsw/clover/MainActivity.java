db88b0dd84b66254d74244708a54795efd932e31
ParkSeungSu <cldkr0401@naver.com>
2020-12-16 16:28:44 +0900

    FIrebase
    upload Message

--- app/src/main/java/halla/icsw/clover/MainActivity.java
+++ app/src/main/java/halla/icsw/clover/MainActivity.java
@@ -17,11 +17,7 @@ import halla.icsw.clover.fragment.TreerFagment;
 public class MainActivity extends AppCompatActivity {
     private String mUsername;
     private BottomNavigationView bottomNavigationView;
-    static SharedPreferences sharedPreferences;
-    static SharedPreferences.Editor editor;
-    static private String SHARE_NAME="SHARE_REF";
 
-    int savedCount;
     MainFragment mainFragment;
     MessageFragment messageFragment;
     TreerFagment treerFagment;
@@ -50,10 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //SharedPrefrence 값 불러오기
-        sharedPreferences=getSharedPreferences(SHARE_NAME,MODE_PRIVATE);
-        editor=sharedPreferences.edit();
-        savedCount=getSavedCount();
+
         mainFragment=new MainFragment();
         messageFragment=new MessageFragment();
         treerFagment=new TreerFagment();
@@ -66,12 +59,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void setSavedCount(){
-        savedCount+=1;
-        editor.putInt("count",-1);
-        editor.commit();
-    }
-    public int getSavedCount(){
-        return sharedPreferences.getInt("count",-1);
-    }
+
 }
--- app/src/main/java/halla/icsw/clover/fragment/TreerFagment.java
+++ app/src/main/java/halla/icsw/clover/fragment/TreerFagment.java
@@ -1,5 +1,6 @@
 package halla.icsw.clover.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,13 +10,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import halla.icsw.clover.MainActivity;
 import halla.icsw.clover.R;
 
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class TreerFagment extends Fragment {
@@ -25,24 +30,85 @@ public class TreerFagment extends Fragment {
     private TextView treeNameText;
     private EditText messageEdit;
     private ImageView treeImage;
+    int savedCount;
+    static private String SHARE_NAME="SHARE_REF";
+
+    static SharedPreferences sharedPreferences;
+    static SharedPreferences.Editor editor;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_tree, container, false);
-
+        sharedPreferences=getActivity().getSharedPreferences(SHARE_NAME,MODE_PRIVATE);
+        editor=sharedPreferences.edit();
         sendButton=view.findViewById(R.id.sendButton);
         treeNameText=view.findViewById(R.id.treeName);
-        treeNameText.setText("TEST TREE");
+        savedCount=getSavedCount();
+
+        treeNameText.setText(String.valueOf(savedCount));
 
         treeImage=view.findViewById(R.id.treeImageView);
+        treeImageSet();
+
         GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(treeImage);
         Glide.with(getActivity()).load(R.drawable.growing).into(gifImage);
 
 
         messageEdit=view.findViewById(R.id.messageEdit);
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadData();
+
+            }
+        });
+
 
         return view;
+    }
+    public void setSavedCount(){
+        savedCount=getSavedCount()+1;
+        editor.putInt("count",savedCount);
+        editor.commit();
+    }
+    public int getSavedCount(){
+        return sharedPreferences.getInt("count",0);
+    }
+    public void treeImageSet(){
+        savedCount=getSavedCount();
+//        if(){
+//
+//        }else if() {
+//
+//        }else if() {
+//
+//        }else if(){
+//
+//        }else if(){
+//
+//        }else if(){
+//
+//        }else {
+//
+//        }
+    }
+    public void uploadData(){
+        String data=messageEdit.getText().toString();
+        if(data != null && !data.equals("") ) {
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference rootRef= firebaseDatabase.getReference();
+            rootRef.setValue(data);
+            setSavedCount();
+            messageEdit.setText("");
+            Toast.makeText(getActivity(), "멋진 메시지에요!", Toast.LENGTH_SHORT).show();
+            treeNameText.setText(String.valueOf(getSavedCount()));
+
+        }else{
+            Toast.makeText(getActivity(), "메시지를 다시한번 확인해 주세요", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
